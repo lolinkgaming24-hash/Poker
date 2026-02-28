9e5939560052eaaeb82260e0449436fbeefd111e
Bertie690 <taylormw163@gmail.com>
2026-02-28 00:00:33 -0500

    dev: `MockConsole` no longer drops extra arguments for colored logs

--- test/mocks/mock-console/infer-color.ts
+++ test/mocks/mock-console/infer-color.ts
@@ -2,36 +2,48 @@ import { hslToHex } from "#utils/common";
 import chalk, { type ChalkInstance, type ForegroundColorName, foregroundColorNames } from "chalk";
 import colorMap from "./color-map.json";
 
+/**
+ * Determine the color to use for a log message based on its arguments, and remove any CSS formatting directives found within.
+ * @param data - The original arguments to the logging function
+ * @returns
+ * The `ChalkInstance` to use to color the output.
+ * @remarks
+ * Used to support CSS color directives in Node.JS environments which lack native support.
+ */
 export function inferColorFormat(data: [string, ...unknown[]]): ChalkInstance {
   // Remove all CSS format strings and find the first one containing something vaguely resembling a color
   data[0] = data[0].replaceAll("%c", "");
-  const args = data.slice(1).filter(t => typeof t === "string");
-  const color = findColorPrefix(args);
+  const [color, index] = findColorPrefix(data);
+  if (index !== -1) {
+    data.splice(index, 1);
+  }
 
-  // If the color is within Chalk's native roster, use it directly.
+  // use color directly if supported, or coerce it to hex otherwise
   if ((foregroundColorNames as string[]).includes(color)) {
     return chalk[color as ForegroundColorName];
   }
 
-  // Otherwise, coerce it to hex before feeding it in.
-  return getColor(color);
+  return parseCSSColor(color);
 }
 
 /**
  * Find the first string with a "color:" CSS directive in an argument list.
  * @param args - The arguments containing the color directive
- * @returns The found color, or `"green"` if none were found
+ * @returns The found color and its position inside the array, or `"green"` if none were found
  */
-function findColorPrefix(args: string[]): string {
-  for (const arg of args) {
+function findColorPrefix(data: unknown[]): [color: string, index: number] {
+  for (const [index, arg] of data.entries()) {
+    if (typeof arg !== "string") {
+      continue;
+    }
     const match = /color:\s*(.+?)(?:;|$)/g.exec(arg);
     if (match === null) {
       continue;
     }
 
-    return match[1];
+    return [match[1], index];
   }
-  return "green";
+  return ["green", -1];
 }
 
 /**
@@ -39,7 +51,7 @@ function findColorPrefix(args: string[]): string {
  * @param color - The color to coerce
  * @returns The Chalk color equivalent.
  */
-function getColor(color: string): ChalkInstance {
+function parseCSSColor(color: string): ChalkInstance {
   if (/^#([a-z0-9]{3,4}|[a-z0-9]{6}|[a-z0-9]{8})$/i.test(color)) {
     // already in hex
     return chalk.hex(color);
