8cc79f9e61d7b246c9580641f7599147d8a25966
Bertie690 <taylormw163@gmail.com>
2026-03-03 01:34:50 -0500

    Fix accidental deletion failing tset

--- test/tests/moves/grudge.test.ts
+++ test/tests/moves/grudge.test.ts
@@ -89,6 +89,7 @@ describe("Moves - Grudge", () => {
 
     game.move.use(MoveId.GUILLOTINE);
     await game.move.forceEnemyMove(MoveId.SPLASH);
+    await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
     await game.toEndOfTurn();
 
     expect(ratatta).toHaveFainted();
