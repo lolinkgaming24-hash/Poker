3d9c24464c374d9aba61a3c1103d125113f9d2b5
macwell <maxwellborgeszulauf@gmail.com>
2026-03-28 00:54:52 -0400

    Move froakie shiny/variant unlocks onto new starter

--- src/system/version-migration/versions/v1_11_17.ts
+++ src/system/version-migration/versions/v1_11_17.ts
@@ -1,4 +1,3 @@
-import { DexAttr } from "#enums/dex-attr";
 import { SpeciesId } from "#enums/species-id";
 import { RibbonData } from "#system/ribbons/ribbon-data";
 import type { SystemSaveData } from "#types/save-data";
@@ -8,7 +7,7 @@ const migrateGreninjaBattleBondForm: SystemSaveMigrator = {
   version: "1.11.17",
   migrate: (data: SystemSaveData): void => {
     if (data.starterData && data.dexData) {
-      // Check whether player has Battle Bond form unlocked on Froakie?
+      // Check whether player has Battle Bond form unlocked on Froakie using DexAttr.DEFAULT_FORM?
       data.starterData[SpeciesId.BATTLE_BOND_GRENINJA] = {
         moveset: data.starterData[SpeciesId.FROAKIE].moveset,
         eggMoves: 0,
@@ -20,12 +19,9 @@ const migrateGreninjaBattleBondForm: SystemSaveMigrator = {
         classicWinCount: 0,
       };
 
-      const defaultStarterAttr =
-        DexAttr.NON_SHINY | DexAttr.MALE | DexAttr.FEMALE | DexAttr.DEFAULT_VARIANT | DexAttr.DEFAULT_FORM;
-
       data.dexData[SpeciesId.BATTLE_BOND_GRENINJA] = {
-        seenAttr: defaultStarterAttr,
-        caughtAttr: defaultStarterAttr,
+        seenAttr: data.dexData[SpeciesId.FROAKIE].seenAttr,
+        caughtAttr: data.dexData[SpeciesId.FROAKIE].caughtAttr,
         natureAttr: data.dexData[SpeciesId.FROAKIE].natureAttr,
         seenCount: 0,
         caughtCount: 0,
